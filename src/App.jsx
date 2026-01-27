e6db4a464054f27e864e133aa43971896913ec0c
GANESH TAKWALE <65443906+ganesh22takwale@users.noreply.github.com>
2026-01-27 20:14:44 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import GRTScene from "./components/GRTScene"
+
+export default function App() {
+  return (
+    <div style={{ height: "100vh", background: "black" }}>
+      {/* Use variant="silver" for GRT-LOGO-11 */}
+      <GRTScene variant="color" />
+    </div>
+  )
+}
