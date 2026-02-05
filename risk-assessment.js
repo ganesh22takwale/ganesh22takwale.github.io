e9ccf8e3c6a46db4184d8379bbcd72a7b7225b33
GANESH TAKWALE <65443906+ganesh22takwale@users.noreply.github.com>
2026-02-05 22:25:13 +0530

    Update risk-assessment.js

--- risk-assessment.js
+++ risk-assessment.js
@@ -1,1 +1,24 @@
+function runRisk(){
+
+  const scale = Number(document.getElementById("scale").value);
+  const downtime = Number(document.getElementById("downtime").value);
+  const recovery = Number(document.getElementById("recovery").value);
+  const human = Number(document.getElementById("human").value);
+
+  const exposure = downtime * recovery * scale;
+  const humanFactor = human * scale;
+
+  const totalRisk = exposure + humanFactor;
+
+  let maturity = "LOW";
+
+  if(totalRisk < 500000) maturity = "MODERATE";
+  if(totalRisk < 250000) maturity = "STRONG";
+
+  document.getElementById("riskOutput").innerHTML = `
+    <h2>Risk Exposure: $${totalRisk.toLocaleString()}</h2>
+    <p>Architecture Maturity Need: ${maturity}</p>
+  `;
+
+}
 
