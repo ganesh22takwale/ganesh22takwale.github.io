a46d03b013dd35cc5ea509691a816126a6367573
GANESH TAKWALE <65443906+ganesh22takwale@users.noreply.github.com>
2026-02-05 22:25:37 +0530

    Update roi-v3-engine.js

--- roi-v3-engine.js
+++ roi-v3-engine.js
@@ -1,1 +1,22 @@
+function calculateROIv3(inputs){
+
+  const annualFailureRisk =
+    inputs.failureCost *
+    inputs.failureHours *
+    inputs.eventsPerYear;
+
+  const architecturePrevention = annualFailureRisk * 0.45;
+
+  const trustMultiplier = architecturePrevention * 0.25;
+
+  const totalROI = architecturePrevention + trustMultiplier;
+
+  return {
+    risk: annualFailureRisk,
+    prevention: architecturePrevention,
+    trustValue: trustMultiplier,
+    totalROI: totalROI
+  };
+
+}
 
