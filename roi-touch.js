5d90f571be6cb8f872172f455f8fa00ecbf1c3ef
GANESH TAKWALE <65443906+ganesh22takwale@users.noreply.github.com>
2026-04-20 21:37:00 +0530

    Delete roi-touch.js

--- roi-touch.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".tier").forEach(t => {
-  t.addEventListener("touchstart", () => {
-    t.style.transform = "scale(1.05)";
-    if (navigator.vibrate) navigator.vibrate(10);
-  });
-  t.addEventListener("touchend", () => {
-    t.style.transform = "scale(1)";
-  });
-});
